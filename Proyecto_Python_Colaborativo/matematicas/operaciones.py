3d93ea326c460dd1d92fa94e13d679173ef75c93
michael <michael.zhang@uip.edu.pa>
2025-10-26 12:00:43 -0500

    se agrego la multiplicacion y la division en el area de matematica

--- Proyecto_Python_Colaborativo/matematicas/operaciones.py
+++ Proyecto_Python_Colaborativo/matematicas/operaciones.py
@@ -7,3 +7,11 @@ def sumar(a, b):
 def restar(a, b):
     """Resta dos números."""
     return a - b
+
+def multiplicar(a, b):
+    """Multiplica dos números."""
+    return a * b
+
+def dividir(a, b):
+    """Divide dos números. Lanza un error si el divisor es cero."""
+    return a / b
