a61b4d6abd9a79c8f4b36c40f67fc044a7e65ede
michael <michael.zhang@uip.edu.pa>
2025-10-26 12:12:39 -0500

    Agregue otra parte al cuento

--- Proyecto_Python_Colaborativo/cuentos/historia.py
+++ Proyecto_Python_Colaborativo/cuentos/historia.py
@@ -4,3 +4,10 @@ def imprimir_cuento_base():
     """Imprime el inicio de la narrativa."""
     print("--- El Viaje del Bit ---")
     print("El pequeño bit buscaba una conexión estable y veloz.")
+    print("En su travesía, encontró varios obstáculos y aliados.")
+    print("Esos aliados le enseñaron sobre la importancia de la colaboración y el trabajo en equipo.") 
+    print("en uno de sus obstáculos, el bit tuvo que aprender a dividir sus recursos para avanzar.") 
+    print("Y al avanzar, descubrió que sumar fuerzas con otros bits hacía el viaje más fácil.")
+
+
+imprimir_cuento_base()
