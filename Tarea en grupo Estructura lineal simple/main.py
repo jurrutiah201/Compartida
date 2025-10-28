9675ffb966ef1c33391c426e420be01189e3b6ee
Michael Zhang <michael.zhang@uip.edu.pa>
2025-10-28 12:49:29 -0500

    se agrego algunos inputs y parte de historia

--- Tarea en grupo Estructura lineal simple/main.py
+++ Tarea en grupo Estructura lineal simple/main.py
@@ -1,1 +1,13 @@
+nombre = input("Ingresa tu nombre: ")
+edad = input("Ingresa tu edad: ")
+ciudad = input("Ingresa tu ciudad de residencia: ")
+trabajo = input("Ingresa tu ocupación o trabajo: ")
+estudio = input("Ingresa tu área de estudio o especialización: ")
 
+
+print("\n-----------------------------------------")
+print(f"¡Hola, soy {nombre}!, tengo {edad} años y vivo en {ciudad}.")
+print(f"Actualmente, trabajo como {trabajo}.")
+print("mi sueño es poder viajar por todo el mundo y conocer nuevas culturas.")
+print(f"Además, estudio {estudio} para mejorar mis habilidades y conocimientos.")
+print("Me encanta aprender cosas nuevas y enfrentar nuevos desafíos cada día.")
