17f7ccb3936d557ac4178da4e162fa33edccb053
DAVID ARTURO ACEVEDO ORDONEZ <david.acevedo@uip.edu.pa>
2025-11-18 19:26:30 -0500

    Refactor difficulty selection to use a loop

--- modulo 6  Ejercicio Adivina el número secreto/juegos/Dificultad.py
+++ modulo 6  Ejercicio Adivina el número secreto/juegos/Dificultad.py
@@ -4,14 +4,15 @@ def seleccionar_dificultad():
     print("2. Medio   (1 - 20)")
     print("3. Dificil (1 - 50)")
 
-    opcion = input("Elige una opción: ")
-
-    if opcion == "1":
-        return 10
-    elif opcion == "2":
-        return 20
-    elif opcion == "3":
-        return 50
-    else:
+   while True:
+        opcion = input("Ingresa el número de opción (1, 2 o 3): ")
+        
+        if opcion == '1':
+            return 10  # Retorna el límite superior
+        elif opcion == '2':
+            return 20
+        elif opcion == '3':
+            return 50
+        else:
         print("Opción inválida. Se elige dificultad media (1–20).")
         return 20
